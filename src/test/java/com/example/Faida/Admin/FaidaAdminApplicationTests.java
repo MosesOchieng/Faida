aa67d3141793f9baa377bd613b90676eb65dc414
MosesOchieng <mosesochiengopiyo@gmail.com>
2023-05-26 15:43:52 +0300

    push commit

---
+++ src/test/java/com/example/Faida/Admin/FaidaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Faida.Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaidaAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
